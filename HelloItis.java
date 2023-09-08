b05dcc6d0c978c7a4016e4df642d0d9a5de9e171
LizaMusina <lisamusina33@gmail.com>
2023-09-08 20:11:00 +0300

    Added welcome

--- HelloItis.java
+++ HelloItis.java
@@ -1,5 +1,5 @@
 class HelloItis {
 	public static void main(String[] args) {
-		System.out.println("HelloItis, dear Itis, bye, Itis!");
+		System.out.println("HelloItis, welcome, dear Itis, bye, Itis!");
 	}
 }
